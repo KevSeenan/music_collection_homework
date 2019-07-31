fde12cea16a59406bbc2d568c9e188b5b793245e
KevSeenan <kev.seenan@gmail.com>
2019-07-31 16:31:45 +0100

    adds save for albums with artists ID added

--- models/albums.rb
+++ models/albums.rb
@@ -1,21 +1,20 @@
 require_relative('../db/sql_runner.rb')
-# require_relative('./artists.rb')
+require_relative('./artists.rb')
 
 class Album
 
-  attr_reader :title, :genre
-
+  attr_reader :title, :genre, :artist_id
   def initialize(album)
     @id = album['id'].to_i if album['id']
     @title = album['title']
     @genre = album['genre']
-    # @artist_id = album['artist_id'].to_i()
+    @artist_id = album['artist_id'].to_i()
   end
 
   def save()
-    sql = "INSERT INTO albums (title, genre)
-    VALUES ($1, $2) RETURNING id"
-    values = [@title, @genre]
+    sql = "INSERT INTO albums (title, genre, artist_id)
+    VALUES ($1, $2, $3) RETURNING id"
+    values = [@title, @genre, @artist_id]
     result = SqlRunner.run(sql, values)
     @id = result[0]['id'].to_i
   end
